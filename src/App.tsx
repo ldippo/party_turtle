19004220e0962a64fe72dcc02b7893c308f72866
Lyle Dippo <ldippo421@gmail.com>
2022-03-08 14:01:10 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+import PartyTurtle from "./PartyTurtle";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h3>Behold, Party Turtle</h3>
+      <PartyTurtle />
+    </div>
+  );
+}
---
+++ src/PartyTurtle.tsx
@@ -0,0 +1,64 @@
+export default function PartyTurtle() {
+  return (
+    <svg
+      id="turtle_masterpiece"
+      data-name="turtle masterpiece"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 614 570.76"
+      style={{ maxWidth: "100%", maxHeight: 500 }}
+    >
+      <title>party turtle</title>
+      <ellipse
+        id="top_right_fin"
+        data-name="top right fin"
+        cx="532.5"
+        cy="192"
+        rx="81.5"
+        ry="31"
+        style={{ fill: "#e2cb27" }}
+      />
+      <ellipse
+        id="top_left_fin"
+        data-name="top left fin"
+        cx="81.5"
+        cy="192"
+        rx="81.5"
+        ry="31"
+        style={{ fill: "#f41414" }}
+      />
+      <ellipse
+        id="shell"
+        cx="306"
+        cy="328.5"
+        rx="153"
+        ry="174.5"
+        style={{ fill: "#29cdff" }}
+      />
+      <ellipse
+        id="bottom_right_fin"
+        data-name="bottom right fin"
+        cx="536.45"
+        cy="604.18"
+        rx="22.41"
+        ry="55.87"
+        transform="translate(-359.85 652.82) rotate(-57.27)"
+        style={{ fill: "#06ff06" }}
+      />
+      <ellipse
+        id="bottom_left_fin"
+        data-name="bottom left fin"
+        cx="272.07"
+        cy="604.18"
+        rx="55.87"
+        ry="22.41"
+        transform="translate(-445.53 293.35) rotate(-45)"
+        style={{ fill: "#4235ed" }}
+      />
+      <path
+        d="M398,146.5l47.67-37C435.49,88.7,417.41,76,398,76c-30.72,0-56,31.82-56,70.5S367.28,217,398,217s56-31.82,56-70.5ZM390,121a14,14,0,1,1,14-14A14,14,0,0,1,390,121Z"
+        transform="translate(-98 -76)"
+        style={{ fill: "#077018" }}
+      />
+    </svg>
+  );
+}
